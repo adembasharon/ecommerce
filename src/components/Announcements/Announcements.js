81231aeb93f95c2a61607b8d3c366157877dabfa
adembasharon <adembasharon816@gmail.com>
2021-11-15 11:28:05 +0300

    created component

---
+++ src/components/Announcements/Announcements.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Announcements.css";
+
+const Announcements=()=>{
+
+    return(
+        <div className="offer">
+            <p>Offer Free Shipping For Producs Above KES.30,000</p>
+          
+        </div>
+    )
+}
+export default Announcements;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Announcements from "../components/Announcements/Announcements";
+
+const Home=()=>{
+    return(
+        <div>
+<Announcements />
+
+</div>
+
+    )
+}
+export default Home;
+
+
+
