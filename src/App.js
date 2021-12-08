126fb3052acb662f44ea91cdf5354617483e05f1
adembasharon <adembasharon816@gmail.com>
2021-12-08 09:53:13 +0300

    added hashreact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import {HashRouter as Router, Routes , Route } from 'react-router-dom';
 import './App.css';
+import React from "react"
 import Home from "./pages/Home"
 import Cart from "./pages/Cart";
 import  Login from "./pages/Login";
