5c22a50135a668b701010a1603968eee5071dab8
adembasharon <adembasharon816@gmail.com>
2021-12-08 12:06:09 +0300

    removed slashes

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export  const slides=[
 {
     id:1,
-    img:"/Images/laptop-woman.jpeg",
+    img:"Images/laptop-woman.jpeg",
     title:"HOLLIDAY OFFER",
     bg:"rgb(223, 223, 183)",
     des:"Grap Our Latest December Offers"
@@ -10,7 +10,7 @@ export  const slides=[
 },
 {
     id:2,
-    img:"/Images/shopping-in-street.jpeg",
+    img:"Images/shopping-in-street.jpeg",
     title:"CLAERANCE SALE",
     bg:"#BFC3C4",
     des:"Get Latest Products As We Claer Our Stores"
@@ -19,7 +19,7 @@ export  const slides=[
 {
 
 id:3,
-img:"/Images/woman-cariyng-shopping bag.jpeg",
+img:"Images/woman-cariyng-shopping bag.jpeg",
 title:"NEW ARRIVALS",
 bg:"#DAD0C8",
 des:"Grab Your Latest Offer As They Arrive In Market "  
